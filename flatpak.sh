95dd12b956554146fbece058158b4fad435d06b4
mikado990 <137442705+mikado990@users.noreply.github.com>
2025-05-01 11:10:27 +0200

    Create flatpak.sh

---
+++ flatpak.sh
@@ -0,0 +1,5 @@
+
+
+flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+
+flatpak install flathub org.onlyoffice.desktopeditors com.spotify.Client it.mijorus.gearlever com.discordapp.Discord
